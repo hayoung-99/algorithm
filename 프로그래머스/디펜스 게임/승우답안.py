ade293e38fc30a305d1cbafa3932243cf02a2c19
handwoo24 <handwoo24@gmail.com>
2023-01-18 18:02:07 +0900

    시간 초과 ㅠ

--- 프로그래머스/디펜스 게임/승우답안.py
+++ 프로그래머스/디펜스 게임/승우답안.py
@@ -1,6 +1,14 @@
+from time import time
+from random import randint
+
+enemy = [randint(1, 1000000) for x in range(1000000)]
+n = 1000000000
+k = 500000
+
+
 def solution(n, k, enemy):
     length = len(enemy)
-    if k > length:
+    if k >= length:
         return length
 
     def calculate(index):
@@ -9,8 +17,12 @@ def solution(n, k, enemy):
         sum_skip_enemy = sum(max_sorted_enemy[:k])
         defeated_enemy = sum_enemy - sum_skip_enemy
         residul_soldier = n - defeated_enemy
-        return index, 0 if residul_soldier >= 0 else 1
+        return index if residul_soldier >= 0 else 0
 
-    sort_func = lambda x: (x[1], -x[0])
-    index, _ = sorted([calculate(i) for i in range(length)], key=sort_func)[0]
+    index = sorted(map(calculate, range(k, length)), reverse=True)[0]
     return index + 1
+
+
+s = time()
+solution(n, k, enemy)
+print(time() - s)
