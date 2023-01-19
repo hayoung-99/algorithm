f12780b4b8c90e71fd6013d00fc7e2dc73e498da
hayoung-99 <nhy990130@gmail.com>
2023-01-19 10:55:22 +0900

    재귀말고 다른 방법 구상중

--- 프로그래머스/디펜스 게임/hayoung.py
+++ 프로그래머스/디펜스 게임/hayoung.py
@@ -1,30 +1,25 @@
-def play_game(enemy, idx, k, soldiers):
-    global answer
-
-    if idx == 0:
-        answer = 0
-
-    if idx < len(enemy):
-        # step 1) 무적권 쓰기
-        if k > 0:
-            play_game(enemy, idx + 1, k - 1, soldiers)
-
-        # step 2) 병사가 enemy[idx] 이상이면 병사 소모
-        if enemy[idx] <= soldiers:
-            play_game(enemy, idx + 1, k, soldiers - enemy[idx])
-
-        # step 3) 병사도 적고 무적권도 없는 경우
-        if (k == 0) and (enemy[idx] > soldiers):
-            if answer < idx:
-                answer = idx
-
-    else:
-        answer = len(enemy)
+import heapq
 
 
 def solution(n, k, enemy):
-    play_game(enemy, 0, k, n)
+    k_list = enemy[:k]
+    heapq.heapify(k_list)
+    answer = 0
+
+    for i in range(k, len(enemy)):
+        print(k_list)
+        if k_list[0] < enemy[i]:
+            cur_k_min = heapq.heappop(k_list)
+            heapq.heappush(k_list, enemy[i])
+
+            n -= cur_k_min
+
+        else:
+            n -= enemy[i]
+
+        if n < 0:
+            answer = i
+            break
 
     print(answer)
 
-    return answer
