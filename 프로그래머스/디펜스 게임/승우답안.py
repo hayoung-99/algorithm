dffb5bb9d4ee1661860329e9624f65172089ea32
handwoo24 <handwoo24@gmail.com>
2023-01-19 20:58:10 +0900

    누계함수 적용

--- 프로그래머스/디펜스 게임/승우답안.py
+++ 프로그래머스/디펜스 게임/승우답안.py
@@ -1,15 +1,18 @@
+from itertools import accumulate
+
+
 def solution(n, k, enemy):
     length = len(enemy)
     if k >= length:
         return length
 
-    def calculate(index):
-        sum_enemy = sum(enemy[: index + 1])
+    def calculate(value: tuple):
+        index, sum_enemy = value
         max_sorted_enemy = sorted(enemy[: index + 1], reverse=True)
         sum_skip_enemy = sum(max_sorted_enemy[:k])
         defeated_enemy = sum_enemy - sum_skip_enemy
         residul_soldier = n - defeated_enemy
         return index if residul_soldier >= 0 else 0
 
-    index = max(map(calculate, range(k, length)))
+    index = max(map(calculate, enumerate(accumulate(enemy))))
     return index + 1
