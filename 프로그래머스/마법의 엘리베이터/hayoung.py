4580fff804d2d61c3881a7dd3361b5a08b598c95
hayoung-99 <nhy990130@gmail.com>
2023-01-21 23:51:59 +0900

    23.1점 로직이 틀림

--- 프로그래머스/마법의 엘리베이터/hayoung.py
+++ 프로그래머스/마법의 엘리베이터/hayoung.py
@@ -1,8 +1,18 @@
 def solution(storey):
-    num = storey
+    num = str(storey)
+    answer = 0
 
-    # 2번째 자릿수(i)부터 탐색해서
-    # 1) num[i] = 0 ~ 5 => num[i] + 1
-    # 2) num[i] = 6 ~ 9 => num[i]
-    # 1)에 부합하면 num = (num[i] + 1) * 10^(자릿수) - num 으로 업데이트
-    # 2)에 부합하면 num = num - (num[i] * 10^(자릿수))으로 업데이트
+    while int(num) > 0:
+        if len(num) == 1:
+            answer += int(num)
+            break
+
+        if 0 <= int(num[1]) <= 5:
+            answer += int(num[0])
+            num = str(int(num) - (int(num[0]) * 10 ** (len(num) - 1)))
+
+        else:
+            answer += (int(num[0]) + 1)
+            num = str((int(num[0]) + 1) * (10 ** (len(num) - 1)) - int(num))
+
+    return answer
