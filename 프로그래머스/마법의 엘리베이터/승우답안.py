396bbbdeea3909046e9afbe26409c25fc199b487
handwoo24 <handwoo24@gmail.com>
2023-01-21 20:25:45 +0900

    실패 케이스 존재 76점

--- 프로그래머스/마법의 엘리베이터/승우답안.py
+++ 프로그래머스/마법의 엘리베이터/승우답안.py
@@ -1,25 +1,23 @@
-from math import log10, floor, pow
+from math import fmod
 
 
 def solution(storey):
     def calculate(acc, storey):
-        log_storey = log10(storey)
-        a = floor(log_storey)
-        A = pow(10, a)
-        b = floor(storey / A)
-        B = int(b * A)
-        r = storey - B
-        if b > 5:
-            b = 10 - b
-            acc += 1
-        acc += b
-        if not r:
-            return acc
-        else:
-            return calculate(acc, r)
+        r = int(fmod(storey, 10))
+        if r > 5:
+            r = 10 - r
+            storey += 10
+        acc += r
+        storey = int(storey / 10)
+        if storey < 10:
+            if storey > 5:
+                return acc + 1 + 10 - storey
+            return acc + storey
+        return calculate(acc, storey)
 
     answer = calculate(0, storey)
     return answer
 
 
 print(solution(2554))
+print(solution(16))
