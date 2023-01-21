0bedeebcb13d82d9d602012fef9d4d244dbf53c4
handwoo24 <handwoo24@gmail.com>
2023-01-21 21:16:59 +0900

    테스트 케이스가 함정이 있군요...

--- 프로그래머스/마법의 엘리베이터/승우답안.py
+++ 프로그래머스/마법의 엘리베이터/승우답안.py
@@ -1,11 +1,12 @@
 def solution(storey):
     def calculate(acc, storey):
         r = storey % 10
-        if r > 5:
+        storey = int((storey - r) / 10)
+        next_r = storey % 10
+        if r > 5 or (next_r >= 5 and r >= 5):
             r = 10 - r
-            storey += 10 - r
+            storey += 1
         acc += r
-        storey = int(storey / 10)
         if storey < 10:
             if storey > 5:
                 return acc + 1 + 10 - storey
