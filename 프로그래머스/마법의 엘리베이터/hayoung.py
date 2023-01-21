bc483c3172a00819858426acc2ad93b7a31a8e20
hayoung-99 <nhy990130@gmail.com>
2023-01-22 01:18:00 +0900

    해결

--- 프로그래머스/마법의 엘리베이터/hayoung.py
+++ 프로그래머스/마법의 엘리베이터/hayoung.py
@@ -3,7 +3,19 @@ def solution(storey):
     answer = 0
 
     while int(num) > 0:
-        if 5 < int(num[0]):
+        if len(num) == 1:
+            if 5 < int(num):
+                answer += 1
+                answer += (10 - int(num))
+            else:
+                answer += int(num)
+            break
+
+        target = int(num[0])
+        if 5 <= int(num[1]):
+            target += 1
+
+        if 5 < target:
             answer += 1
             num = str(10 ** len(num) - int(num))
 
