70e5c610ffb82f3e653f32339b68aa776621b26e
hayoung-99 <nhy990130@gmail.com>
2023-01-22 00:25:28 +0900

    76.9점 띠용

--- 프로그래머스/마법의 엘리베이터/hayoung.py
+++ 프로그래머스/마법의 엘리베이터/hayoung.py
@@ -7,16 +7,22 @@ def solution(storey):
         num = str((10 ** len(num)) - storey)
 
     while int(num) > 0:
+        # print(f"{num}, {answer}")
         if len(num) == 1:
             answer += int(num)
             break
 
-        if 0 <= int(num[1]) <= 5:
-            answer += int(num[0])
-            num = str(int(num) - (int(num[0]) * 10 ** (len(num) - 1)))
+        if 6 <= int(num[0]) <= 9:
+            answer += 1
+            num = str((10 ** len(num)) - int(num))
 
         else:
-            answer += (int(num[0]) + 1)
-            num = str((int(num[0]) + 1) * (10 ** (len(num) - 1)) - int(num))
+            if 0 <= int(num[1]) <= 5:
+                answer += int(num[0])
+                num = str(int(num) - (int(num[0]) * 10 ** (len(num) - 1)))
+
+            else:
+                answer += (int(num[0]) + 1)
+                num = str((int(num[0]) + 1) * (10 ** (len(num) - 1)) - int(num))
 
     return answer
