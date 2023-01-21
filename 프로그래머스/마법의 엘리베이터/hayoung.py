325820e0818ceba5ef33aeb4dbdee78b7bb20af9
hayoung-99 <nhy990130@gmail.com>
2023-01-22 00:53:10 +0900

    코드 정리

--- 프로그래머스/마법의 엘리베이터/hayoung.py
+++ 프로그래머스/마법의 엘리베이터/hayoung.py
@@ -2,26 +2,14 @@ def solution(storey):
     num = str(storey)
     answer = 0
 
-    while True:
-        if len(num) == 1:
-            if int(num) <= 5:
-                answer += int(num)
-            else:
-                answer += 1
-                answer += (10 - int(num))
-            break
-
-        if 6 <= int(num[0]):
+    while int(num) > 0:
+        if 5 < int(num[0]):
             answer += 1
-            num = str((10 ** len(num)) - int(num))
+            num = str(10 ** len(num) - int(num))
 
         else:
-            if int(num[1]) <= 5:
-                answer += int(num[0])
-                num = str(int(num) - (int(num[0]) * 10 ** (len(num) - 1)))
+            answer += int(num[0])
+            num = str(int(num) - int(num[0]) * (10 ** (len(num) - 1)))
 
-            else:
-                answer += (int(num[0]) + 1)
-                num = str((int(num[0]) + 1) * (10 ** (len(num) - 1)) - int(num))
-
+    print(answer)
     return answer
