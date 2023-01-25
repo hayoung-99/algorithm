ab0da3b943cd4fca695903261a3d549dd1c78964
handwoo24 <handwoo24@gmail.com>
2023-01-25 20:34:31 +0900

    60분경과 ㅠ

--- 프로그래머스/이모티콘 할인행사/승우답안.py
+++ 프로그래머스/이모티콘 할인행사/승우답안.py
@@ -1,4 +1,4 @@
-from itertools import filterfalse
+from itertools import product, filterfalse
 
 
 def solution(users, emoticons):
@@ -13,11 +13,27 @@ def solution(users, emoticons):
 
     valid_ratio = [10, 20, 30, 40]
     plus_users = list(filter(user_max_plan, users))
-    purchase_users = list(filterfalse(user_max_plan, users))
+
+    ratio_cases = list(product(valid_ratio, repeat=len(emoticons)))
+
+    def emoticon_cost(pair, case):
+        index = pair[0]
+        emoticon = pair[1]
+        ratio = case[index]
+        target_user = len(
+            list(filter(lambda u: u[0] <= ratio and not user_max_plan(u), users))
+        )
+        return int(emoticon * (1 - ratio / 100)) * target_user
+
+    def sum_of_emoticon(case):
+        sum_func = lambda e: emoticon_cost(e, case)
+        return sum(map(sum_func, enumerate(emoticons)))
+
+    valid_case = max(ratio_cases, key=lambda c: sum_of_emoticon(c))
 
     # 판매액 구하는 로직은 아예 틀린듯
 
-    answer = [len(plus_users), 0]
+    answer = [len(plus_users), valid_case]
     return answer
 
 
