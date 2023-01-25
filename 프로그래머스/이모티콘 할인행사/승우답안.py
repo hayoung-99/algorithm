fc8501369762f346f09a76c2a12e622a785e49b8
handwoo24 <handwoo24@gmail.com>
2023-01-25 20:04:53 +0900

    판매액 로직 초기화

--- 프로그래머스/이모티콘 할인행사/승우답안.py
+++ 프로그래머스/이모티콘 할인행사/승우답안.py
@@ -14,20 +14,10 @@ def solution(users, emoticons):
     valid_ratio = [10, 20, 30, 40]
     plus_users = list(filter(user_max_plan, users))
     purchase_users = list(filterfalse(user_max_plan, users))
-    min_ratio = max(plus_users, key=lambda x: x[0])[0] if plus_users else 100
-    ## 바로 윗줄 구하는 식을 좀 더 구현해야함
-    print(min_ratio, purchase_users)
 
-    def sum_of_purchase(ratio, emoticon):
-        target_users = list(filter(lambda u: u[0] <= ratio, purchase_users))
-        return int((1 - ratio / 100) * emoticon * len(target_users))
+    # 판매액 구하는 로직은 아예 틀린듯
 
-    def emoticon_plan(emoticon):
-        valid_ratios = [r for r in valid_ratio if r > min_ratio]
-        sum_func = lambda r: sum_of_purchase(r, emoticon)
-        return max(list(map(sum_func, valid_ratios))) if valid_ratios else 0
-
-    answer = [len(plus_users), max(list(map(emoticon_plan, emoticons)))]
+    answer = [len(plus_users), 0]
     return answer
 
 
