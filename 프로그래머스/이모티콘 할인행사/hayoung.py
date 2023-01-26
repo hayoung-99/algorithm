7ed0166aa5f0c530c222c1f02baa0884a90b4495
hayoung-99 <nhy990130@gmail.com>
2023-01-26 16:14:51 +0900

    50분 소요

--- 프로그래머스/이모티콘 할인행사/hayoung.py
+++ 프로그래머스/이모티콘 할인행사/hayoung.py
@@ -1,3 +1,5 @@
+import math
+
 plusUsers, totalPrice = 0, 0
 
 
@@ -10,17 +12,21 @@ def getPrices(users, emoticons, prices, i):
         curTotalPrice = 0
         for user in users:
             tempPrice = 0
-            discount = math.floor(user[0] / 10) * 10
+            discount = math.ceil(user[0] / 10) * 10
             limit = user[1]
+
+            if discount == 0:
+                discount = 10
 
             for k in range(discount, 41, 10):
                 tempPrice += sum(prices[k])
 
-            if limit >= result:
+            if limit <= tempPrice:
                 curPlusUsers += 1
             else:
                 curTotalPrice += tempPrice
 
+        # plusUsers, totalPrice 업데이트
         if plusUsers < curPlusUsers:
             plusUsers = curPlusUsers
             totalPrice = curTotalPrice
@@ -31,19 +37,22 @@ def getPrices(users, emoticons, prices, i):
 
         return
 
-    # 10%
-
-    # 20%
-
-    # 30%
-
-    # 40%
+    # 10% ~ 40% 할인 적용
+    for d in range(10, 41, 10):
+        price = int(emoticons[i] / 100) * (100 - d)
+        prices[d].append(price)
+        getPrices(users, emoticons, prices, i + 1)
+        prices[d].pop()  # 42줄의 append를 되돌리기
 
 
 def solution(users, emoticons):
-    prices = {}
+    prices = {
+        10: [],
+        20: [],
+        30: [],
+        40: []
+    }
 
+    getPrices(users, emoticons, prices, 0)
 
-
-
-
+    return [plusUsers, totalPrice]
