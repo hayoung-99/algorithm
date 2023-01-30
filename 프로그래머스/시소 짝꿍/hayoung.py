ad4dc9673158eaadb73f1e60802956ee4abe2060
hayoung-99 <nhy990130@gmail.com>
2023-01-30 11:13:42 +0900

    76.5점 - 이진탐색으로 접근

--- 프로그래머스/시소 짝꿍/hayoung.py
+++ 프로그래머스/시소 짝꿍/hayoung.py
@@ -1,14 +1,45 @@
+def getLengthByBSearch(weights, idx, rate):
+    length = 0
+    target = weights[idx] * rate[0] / rate[1]
+    # print(f"현재 idx={idx}, target={target}")
+    left = idx + 1
+    right = len(weights) - 1
+
+    while left <= right:
+        mid = int((left + right) / 2)
+        if weights[mid] == target:
+            # 해당 target(중복) 개수 구하기
+            for l in range(mid, idx, -1):  # 왼쪽
+                if weights[l] == target:
+                    length += 1
+                else:
+                    break
+            for r in range(mid + 1, len(weights)):  # 오른쪽
+                if weights[r] == target:
+                    length += 1
+                else:
+                    break
+            # print(f"{target} 발견: 개수 ({length})개")
+            return length
+
+        elif weights[mid] > target:
+            right = mid - 1
+
+        else:
+            left = mid + 1
+
+    return length  # return 0
+
+
 def solution(weights):
     answer = 0
-    for i in range(len(weights)):
-        dic = {}
-        for k in range(2, 5):
-            dic[weights[i] * k] = True
+    rates = [(1, 1), (3, 2), (4, 2), (4, 3)]  # 1:1, 2:3, 2:4, 3:4
 
-        for j in range(i + 1, len(weights)):
-            for w in range(2, 5):
-                if (weights[j] * w) in dic:
-                    answer += 1
-                    break
+    weights.sort()
+
+    for i in range(len(weights)):
+        # 이진 탐색으로 쌍 구하기
+        for rate in rates:
+            answer += getLengthByBSearch(weights, i, rate)
 
     return answer
