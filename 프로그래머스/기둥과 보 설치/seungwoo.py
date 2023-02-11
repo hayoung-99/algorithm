bfa69eea3c7bbabe08d452b209523cf3a6cb511f
handwoo24 <handwoo24@gmail.com>
2023-02-11 23:47:36 +0900

    60분 경과 디버깅중..!

--- 프로그래머스/기둥과 보 설치/seungwoo.py
+++ 프로그래머스/기둥과 보 설치/seungwoo.py
@@ -1,39 +1,66 @@
-world = {}
 answer = []
 
 
 def is_relative_frame(build_frame, installed_frame):
     [x, y, a, _] = build_frame
-    [x1, y1, a1, _] = installed_frame
+    [x1, y1, a1] = installed_frame
     if a and a1:
-        return (x1 == x or x1 == x + 1) and y == y1
+        return (x1 + 1 == x or x1 == x + 1) and y == y1
     elif a:
         return (x1 == x or x1 == x + 1) and (y == y1 or y == y1 + 1)
     elif a1:
-        return (x1 == x or x1 == x + 1) and (y + 1 == y1 or y == y1)
+        return (x1 == x or x1 + 1 == x) and (y + 1 == y1 or y == y1)
     return x1 == x and (y1 == y + 1 or y1 + 1 == y)
 
 
+def is_base_frame(build_frame, installed_frame):
+    [x, y, a, _] = build_frame
+    [x1, y1, a1] = installed_frame
+    if a and a1:
+        return y == y1 and (x + 1 == x1 or x1 + 1 == x)
+    elif a:
+        return y1 + 1 == y and (x + 1 == x1 or x1 == x)
+    elif a1:
+        return y1 == y and (x1 + 1 == x or x1 == x)
+    return y1 + 1 == y
+
+
 def inspect_remove_pillar(build_frame):
-    [x, y, _, _] = build_frame
-    relatives = list(filter(is_relative_frame, answer))
+    relatives = list(filter(lambda f: is_relative_frame(build_frame, f), answer))
+    bases = list(filter(lambda f: is_base_frame(build_frame, f), relatives))
+    if len(bases) == len(relatives):
+        return True
+    return False
 
 
 def inspect_remove_beam(build_frame):
-    [x, y, _, _] = build_frame
-    relatives = list(filter(is_relative_frame, answer))
+    relatives = list(filter(lambda f: is_relative_frame(build_frame, f), answer))
+    bases = list(filter(lambda f: is_base_frame(build_frame, f), relatives))
+    if len(bases) == len(relatives):
+        return True
+    return False
 
 
 def inspect_install_pillar(build_frame):
-    [x, y, _, _] = build_frame
+    [_, y, _, _] = build_frame
     if not y:
-        answer.append(build_frame[:3])
-    relatives = list(filter(is_relative_frame, answer))
+        return True
+    relatives = list(filter(lambda f: is_relative_frame(build_frame, f), answer))
+    bases = list(filter(lambda f: is_base_frame(build_frame, f), relatives))
+    print(bases, build_frame)
+    if bases:
+        return True
+    return False
 
 
 def inspect_install_beam(build_frame):
-    [x, y, _, _] = build_frame
-    relatives = list(filter(is_relative_frame, answer))
+    relatives = list(filter(lambda f: is_relative_frame(build_frame, f), answer))
+    bases = list(filter(lambda f: is_base_frame(build_frame, f), relatives))
+    base_pillar = list(filter(lambda f: not f[2], bases))
+    base_beam = list(filter(lambda f: f[2], bases))
+    if bases and (base_pillar or len(base_beam) == 2):
+        return True
+    return False
 
 
 def inspect(build_frame):
@@ -64,29 +91,41 @@ def build(build_frame):
         remove(build_frame)
 
 
-def build_earth(n):
-    grind = lambda i: world.update({f"{i}-0": 1.0})
-    list(map(grind, range(n + 1)))
-
-
 def solution(n, build_frame):
-    build_earth(n)
     list(map(build, build_frame))
-    return answer
+    return sorted(answer, key=lambda f: (f[0], f[1], f[2]))
 
 
-solution(
-    5,
-    [
-        [1, 0, 0, 1],
-        [1, 1, 1, 1],
-        [2, 1, 0, 1],
-        [2, 2, 1, 1],
-        [5, 0, 0, 1],
-        [5, 1, 0, 1],
-        [4, 2, 1, 1],
-        [3, 2, 1, 1],
-    ],
+print(
+    solution(
+        5,
+        [
+            [1, 0, 0, 1],
+            [1, 1, 1, 1],
+            [2, 1, 0, 1],
+            [2, 2, 1, 1],
+            [5, 0, 0, 1],
+            [5, 1, 0, 1],
+            [4, 2, 1, 1],
+            [3, 2, 1, 1],
+        ],
+    )
 )
 
-print(world)
+print(
+    solution(
+        5,
+        [
+            [0, 0, 0, 1],
+            [2, 0, 0, 1],
+            [4, 0, 0, 1],
+            [0, 1, 1, 1],
+            [1, 1, 1, 1],
+            [2, 1, 1, 1],
+            [3, 1, 1, 1],
+            [2, 0, 0, 0],
+            [1, 1, 1, 0],
+            [2, 2, 0, 1],
+        ],
+    )
+)
