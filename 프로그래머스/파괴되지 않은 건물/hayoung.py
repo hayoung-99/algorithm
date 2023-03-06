121d41c7a18239a0eb515c2baeb8626f8f6de501
hayoung-99 <nhy990130@gmail.com>
2023-03-06 10:25:29 +0900

    53.8점 - 각 행에 대한 누적합으로 접근

--- 프로그래머스/파괴되지 않은 건물/hayoung.py
+++ 프로그래머스/파괴되지 않은 건물/hayoung.py
@@ -1,6 +1,8 @@
 def solution(board, skill):
-    r, c = len(board), len(board[0])
-    buildings = r * c
+    h, w = len(board), len(board[0])
+    buildings = h * w
+
+    total_sum_map = [[0 for _ in range(w + 1)] for _ in range(h)]
 
     for sk in skill:
         r1, c1, r2, c2 = tuple(sk[1:-1])
@@ -8,16 +10,20 @@ def solution(board, skill):
         # 1. 공격
         if sk[0] == 1:
             for x in range(r1, r2 + 1):
-                for y in range(c1, c2 + 1):
-                    board[x][y] -= degree
-                    if board[x][y] + degree > 0 and board[x][y] <= 0:
-                        buildings -= 1
+                total_sum_map[x][c1] -= degree
+                total_sum_map[x][c2 + 1] += degree
+
         # 2. 회복
         elif sk[0] == 2:
             for x in range(r1, r2 + 1):
-                for y in range(c1, c2 + 1):
-                    board[x][y] += degree
-                    if board[x][y] - degree <= 0 and board[x][y] > 0:
-                        buildings += 1
+                total_sum_map[x][c1] += degree
+                total_sum_map[x][c2 + 1] -= degree
+
+    for x in range(h):
+        curr_total = 0
+        for y in range(w):
+            curr_total += total_sum_map[x][y]
+            if board[x][y] + curr_total <= 0:
+                buildings -= 1
 
     return buildings
