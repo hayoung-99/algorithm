e8a9424b4f0550556cb1e7a8f1daca285ca26059
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-10-22 00:04:56 +0900

    [level 2] Title: 삼각 달팽이, Time: 78.87 ms, Memory: 41.3 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -1,41 +1,38 @@
 def solution(n):
-    pos = [[0 for _ in range(i)] for i in range(1, n+1)]
-
-    end = n * (n + 1) // 2
+    end = (n * (n + 1)) // 2
+    answer = [0 for _ in range(end)]
     
-    num = 1
-    curr_col = 0
-    steps = n
     direction = 0  # 0: down, 1: right, 2: up
+    curr_col = 0
+    idx = 0
+    steps = n
+    num = 1
+    
     while num <= end:
+        # 1. 행 칸만큼 앞으로 이동
         if direction == 0:
             for _ in range(steps):
+                idx += curr_col
+                answer[idx] = num
                 curr_col += 1
-                for i in range(curr_col):
-                    if pos[curr_col - 1][i] == 0:
-                        pos[curr_col - 1][i] = num
-                        num += 1
-                        break
+                num += 1
+            
         elif direction == 1:
-            for i in range(curr_col):
-                if pos[curr_col - 1][i] == 0:
-                    pos[curr_col - 1][i] = num
-                    num += 1
+            for _ in range(steps):
+                idx += 1
+                answer[idx] = num
+                num += 1
+                
+        # 2. 현재 idx에서 steps만큼 진행
         else:
             for _ in range(steps):
+                idx -= curr_col
+                answer[idx] = num
                 curr_col -= 1
-                for i in range(curr_col - 1, -1, -1):
-                    if pos[curr_col - 1][i] == 0:
-                        pos[curr_col - 1][i] = num
-                        num += 1
-                        break
-        
-        steps -= 1
+                num += 1
+                
+        # 3. 행 칸만큼 뒤로 이동
         direction = (direction + 1) % 3
-        
-    answer = []
-    for i in pos:
-        for k in i:
-            answer.append(k)
-            
+        steps -= 1
+
     return answer
