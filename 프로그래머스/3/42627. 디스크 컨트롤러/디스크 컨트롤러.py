c46342eb580ecc870d7312eb57fc131786ecdb8e
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-10-27 01:13:56 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -3,17 +3,17 @@ import heapq
 def solution(jobs):
     answer = 0
     total_jobs = len(jobs)
-    jobs.sort()  # 시작 시간이 빠른 순서대로 정렬
+    jobs.sort(key=lambda x: (x[0], x[1]), reverse=True)  # jobs.pop을 빠르게 하기 위해 시작 시간이 빠른 순서로 '내림차순' 정렬
     
-    curr_time = jobs[0][1] + jobs[0][0]
-    answer += jobs[0][1]
-    jobs.pop(0)
+    curr_time = jobs[-1][1] + jobs[-1][0]
+    answer += jobs[-1][1]
+    jobs.pop(-1)
     
     while jobs:
         min_heap = []
         # 1. curr_time 이전에 들어온 요청을 최소힙에 push
-        for idx, job in enumerate(jobs):
-            start, task = job
+        for idx in range(len(jobs)-1, -1, -1):
+            start, task = jobs[idx]
             if start <= curr_time:
                 heapq.heappush(min_heap, (task, idx))
         
@@ -24,8 +24,8 @@ def solution(jobs):
             jobs.pop(idx)
         else:
             # 2. 최소힙이 비어 있다면, curr_time 이후에 들어온 요청 중 맨 처음 요청을 처리
-            answer += jobs[0][1]
-            curr_time = jobs[0][0] + jobs[0][1]
-            jobs.pop(0)
+            answer += jobs[-1][1]
+            curr_time = jobs[-1][0] + jobs[-1][1]
+            jobs.pop(-1)
             
     return answer // total_jobs
