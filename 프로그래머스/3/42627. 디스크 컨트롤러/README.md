dda814192dfef14c0d3648644d3c8b319ab5447d
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-10-27 00:58:27 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,6 +1,6 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 23:38:13
+2023년 10월 5일 0:58:26
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -2,8 +2,8 @@ import heapq
 
 def solution(jobs):
     answer = 0
-    length = len(jobs)
-    jobs.sort()  # 시작 시간이 빠른 순서대로 오름차순 정렬
+    total_jobs = len(jobs)
+    jobs.sort()  # 시작 시간이 빠른 순서대로 정렬
     
     curr_time = jobs[0][1] + jobs[0][0]
     answer += jobs[0][1]
@@ -11,28 +11,21 @@ def solution(jobs):
     
     while jobs:
         min_heap = []
-        for i in range(len(jobs)):
-            task = jobs[i][1]
-            heapq.heappush(min_heap, (task, i))
-                
-        late_task = True
-        task, idx = 0, 0
-        while min_heap:
-            t, i = heapq.heappop(min_heap)
-            if jobs[i][0] >= curr_time:
-                if task == 0 or jobs[i][0] < jobs[idx][0]:
-                    task, idx = t, i
-                
-            else:
-                answer += t + curr_time - jobs[i][0]
-                curr_time += t
-                jobs.pop(i)
-                late_task = False
-                break
-                
-        if late_task:
-            answer += task
-            curr_time = task + jobs[idx][0]
+        # 1. curr_time 이전에 들어온 요청을 최소힙에 push
+        for idx, job in enumerate(jobs):
+            start, task = job
+            if start <= curr_time:
+                heapq.heappush(min_heap, (task, idx))
+        
+        if min_heap:
+            task, idx = heapq.heappop(min_heap)
+            answer += (curr_time + task - jobs[idx][0])
+            curr_time += task
             jobs.pop(idx)
+        else:
+            # 2. 최소힙이 비어 있다면, curr_time 이후에 들어온 요청 중 맨 처음 요청을 처리
+            answer += jobs[0][1]
+            curr_time = jobs[0][0] + jobs[0][1]
+            jobs.pop(0)
             
-    return answer // length
+    return answer // total_jobs
