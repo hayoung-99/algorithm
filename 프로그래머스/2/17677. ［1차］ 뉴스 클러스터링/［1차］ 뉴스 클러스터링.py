9a9a295e11023494ccc8ed8c43056952b062246e
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-10-30 19:21:50 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,12 +1,11 @@
 # 1. 구할 수 있는 영문쌍만 필터링 & 대문자 치환
 # 2. set를 이용해 교집합 & 합집합 구하기
 # 3. min, max 구해서 자카드 유사도 구하기
-import re
 import math
+
 def solution(str1, str2):
-    
-    str1_set = [str1[i:i+2].upper() for i in range(len(str1)-1) if not re.findall('[^a-zA-Z]', str1[i:i+2])]
-    str2_set = [str2[i:i+2].upper() for i in range(len(str2)-1) if not re.findall('[^a-zA-Z]', str2[i:i+2])]
+    str1_set = [str1[i:i+2].upper() for i in range(len(str1)-1) if ('A' <= str1[i].upper() <= 'Z' and 'A' <= str1[i+1].upper() <= 'Z')]
+    str2_set = [str2[i:i+2].upper() for i in range(len(str2)-1) if ('A' <= str2[i].upper() <= 'Z' and 'A' <= str2[i+1].upper() <= 'Z')]
     
     intersection = set(str1_set) & set(str2_set)
     union = set(str1_set) | set(str2_set)
