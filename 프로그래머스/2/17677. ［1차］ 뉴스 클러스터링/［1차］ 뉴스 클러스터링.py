64a4ebc6462f5e8b65e38c0ab3e42813ecff00a4
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-10-30 19:16:11 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,65 +1,22 @@
-# 1. 알파벳을 모두 대문자로 치환한 뒤, 구할 수 있는 영문쌍만 필터링
-# 2. dict로 mix, max 구해서 자카드 유사도 구하기
-def getJaccardSimilarity(str1, str2):
-    str1_dict = {}
-    str2_dict = {}
-    
-    for s1 in str1:
-        if s1 not in str1_dict:
-            str1_dict[s1] = 1
-        else:
-            str1_dict[s1] += 1
-    
-    for s2 in str2:
-        if s2 not in str2_dict:
-            str2_dict[s2] = 1
-        else:
-            str2_dict[s2] += 1
-            
-    # 교집합
-    answer1 = 0
-    intersection = set(str1) & set(str2)
-    for i in intersection:
-        answer1 += min(str1_dict[i], str2_dict[i])
-    
-    # 합집합
-    answer2 = 0
-    union = set(str1) | set(str2)
-    for u in union:
-        # 1. set1에만 있는 경우
-        if u in str1_dict and u not in str2_dict:
-            answer2 += str1_dict[u]
-            continue
-        # 2. str2_dict에만 있는 경우
-        if u in str2_dict and u not in str1_dict:
-            answer2 += str2_dict[u]
-            continue
-        # 3. str1_dict, str2_dict 모두에 있는 경우 
-        answer2 += max(str1_dict[u], str2_dict[u])
-        
-    if answer2 == 0:
-        return 1
-    return answer1 / answer2
-
-
+# 1. 구할 수 있는 영문쌍만 필터링 & 대문자 치환
+# 2. set를 이용해 교집합 & 합집합 구하기
+# 3. min, max 구해서 자카드 유사도 구하기
+import re
+import math
 def solution(str1, str2):
-    import math
     
-    str1 = str1.upper()
-    str2 = str2.upper()
+    str1_set = [str1[i:i+2].upper() for i in range(len(str1)-1) if not re.findall('[^a-zA-Z]', str1[i:i+2])]
+    str2_set = [str2[i:i+2].upper() for i in range(len(str2)-1) if not re.findall('[^a-zA-Z]', str2[i:i+2])]
     
-    str1_set = []
-    str2_set = []
+    intersection = set(str1_set) & set(str2_set)
+    union = set(str1_set) | set(str2_set)
     
-    for i in range(len(str1)-1):
-        curr = str1[i:i+2]
-        if 'A' <= curr[0] <= 'Z' and 'A' <= curr[1] <= 'Z':
-            str1_set.append(curr)
-    for i in range(len(str2)-1):
-        curr = str2[i:i+2]
-        if 'A' <= curr[0] <= 'Z' and 'A' <= curr[1] <= 'Z':
-            str2_set.append(curr)
-            
-    answer = getJaccardSimilarity(str1_set, str2_set)
-    return math.floor(answer * 65536)
+    # 공집합인 경우
+    if not union:
+        return 65536
+    
+    jac_intersection = [min(str1_set.count(i), str2_set.count(i)) for i in intersection]
+    jac_union = [max(str1_set.count(u), str2_set.count(u)) for u in union]
+    
+    return math.floor(sum(jac_intersection) / sum(jac_union) * 65536)
         
