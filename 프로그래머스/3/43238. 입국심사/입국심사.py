84a4e84ed754fe29b03891cdcabd3f5627d70275
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-10-31 10:52:40 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,23 @@
+import sys
+
+def solution(n, times): 
+    answer = 0
+    s, e = 0, sys.maxsize
+    
+    while s <= e:
+        m = (s + e) // 2
+        
+        curr_n = 0
+        for t in times:
+            curr_n += m // t
+            
+            if curr_n >= n:
+                break
+                
+        if curr_n >= n:
+            answer = m
+            e = m - 1
+        else:
+            s = m + 1
+            
+    return answer
