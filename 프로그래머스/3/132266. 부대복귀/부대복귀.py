637fe8342d8e2ddc358d6a4282ecd8435af24002
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-02 11:04:21 +0900

    [level 3] Title: 부대복귀, Time: 125.39 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def solution(n, roads, sources, destination):
+    # 인접 리스트 만들기
+    inj = [[] for _ in range(n+1)]
+    for s, e in roads:
+        inj[s].append(e)
+        inj[e].append(s)
+    
+    # BFS
+    q = deque()
+    q.append((destination, 0))
+    distances = [-1 for _ in range(n+1)]
+    visited = [0 for _ in range(n+1)]
+    distances[destination] = 0
+    visited[destination] = 1
+    while q:
+        curr, dist = q.popleft()
+        for num in inj[curr]:
+            if not visited[num]:
+                visited[num] = 1
+                distances[num] = dist + 1
+                q.append((num, dist+1))
+                
+    answer = []
+    for source in sources:
+        answer.append(distances[source])
+    return answer
