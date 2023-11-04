3ff987da5076f09dd3aa1f2e7578d410e6c83497
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-04 13:01:46 +0900

    [level 2] Title: 피로도, Time: 0.25 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,35 +1,36 @@
 # from itertools import permutations
 
-# def solution(k, dungeons):
-#     answer = 0
+def permutations(arr, n):
+    result = []
     
-#     for pmt in permutations(dungeons, len(dungeons)):
-#         # 완전 탐색
-#         p = k
-#         temp = 0
-#         for need_p, use_p in pmt:
-#             if p >= need_p:
-#                 temp += 1
-#                 p -= use_p
-#             else:
-#                 break
-#         answer = max(answer, temp)
-                
-#     return answer
-
-from itertools import permutations
+    if n == 1:
+        for i in arr:
+            result.append([i])
+        return result
+    
+    if n > 1:
+        for i in range(len(arr)):
+            curr = [i for i in arr]
+            curr.remove(curr[i])
+            for p in permutations(curr, n-1):
+                result.append([arr[i]] + p)
+            
+    return result
+    
 
 def solution(k, dungeons):
-    p = list(permutations(dungeons, len(dungeons)))
-    
     answer = 0
-    for pp in p:
-        kk = k
+    
+    for pmt in permutations(dungeons, len(dungeons)):
+        # 완전 탐색
+        p = k
         temp = 0
-        for need, reduce in pp:
-            if kk >= need:
+        for need_p, use_p in pmt:
+            if p >= need_p:
                 temp += 1
-                kk -= reduce
+                p -= use_p
+            else:
+                break
         answer = max(answer, temp)
         
     return answer
