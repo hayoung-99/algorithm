7b3c6b568bfd5b7526c57c13f6688d95d517bbcd
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-07 12:54:22 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,74 +1,79 @@
-import heapq
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+cost = 99999999999
 
-def inRange(x, y, r):
-    return 0 <= x < r and 0 <= y < r
-
+def DFS(board, curmoney,direct,x,y, length, waycost) :
+    global cost
+    board[x][y] = 1
+    if waycost[x][y] == -1 :
+        waycost[x][y] = curmoney
+    else :
+        if waycost[x][y]+400 < curmoney :
+            return
+        else : 
+            waycost[x][y] = curmoney
+    if (x, y) == (length-1,length-1) :
+        if cost>curmoney :
+            cost = curmoney
+        return
+    direction = [(1,0),(0,1),(-1,0),(0,-1)]
+    for d in direction :
+        newx = x+d[0]
+        newy = y +d[1]
+        if max(newx,newy)<length and min(newx,newy) >=0 and board[newx][newy] == 0:
+            newcurmoney = curmoney + 100 if direct == d else curmoney + 600
+            DFS(board, newcurmoney, d, newx,newy, length, waycost)
+            board[newx][newy] = 0
 
 def solution(board):
-    mapper = {
-        'top': 0,
-        'right': 1,
-        'bottom': 2,
-        'left': 3
-    }
-    dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1]
-    
-    bfs_map = [[0 for _ in range(len(board[0]))] for _ in range(len(board))]
-    q = []
-    
-    # 출발점에서 오른쪽 / 아래 진행
-    if board[0][1] == 0:
-        bfs_map[0][1] = (0, 1, 1, [mapper['right']])
-        heapq.heappush(q, bfs_map[0][1])  # (x, y, total_cost, direction)
-    if board[1][0] == 0:
-        bfs_map[0][1] = (1, 0, 1, [mapper['bottom']])
-        heapq.heappush(q, bfs_map[0][1])
-        
-    while q:
-        x, y, cost, directions = heapq.heappop(q)
-        # print(f"현 지점 {x}, {y}")
-        for dx, dy in zip(dxs, dys):
-            if dx == 1:
-                curr_direction = 'bottom'
-            elif dx == -1:
-                curr_direction = 'top'
-            else:
-                if dy == 1:
-                    curr_direction = 'right'
-                else:
-                    curr_direction = 'left'
-                    
-            if inRange(x + dx, y + dy, len(board)) and board[x + dx][y + dy] == 0:
-                if x + dx == 0 and y + dy == 0:
-                    continue
-                for d in directions:
-                    # 직선 도로 or 코너 도로 => cost 업데이트
-                    if d == 0 or d == 2:
-                        if dx == -1 or dx == 1:
-                            curr_cost = cost + 1
-                        else:
-                            curr_cost = cost + 6
-                            
-                    else:
-                        if dy == 1 or dy == -1:
-                            curr_cost = cost + 1
-                        else:
-                            curr_cost = cost + 6
-                            
-                    if bfs_map[x + dx][y + dy] == 0:
-                        # print(f"(1) 기존 방향 {d}, 가려는 방향 {curr_direction} cost={curr_cost}")
-                        bfs_map[x + dx][y + dy] = (x + dx, y + dy, curr_cost, [mapper[curr_direction]])
-                        heapq.heappush(q, bfs_map[x + dx][y + dy])
-                    else:
-                        prev_cost = bfs_map[x + dx][y + dy][2]
-                        if prev_cost >= curr_cost:
-                            # print(f"(2) 기존 방향 {d}, 가려는 방향 {curr_direction} cost={curr_cost}")
-                            bfs_map[x + dx][y + dy] = (x + dx, y + dy, curr_cost, [mapper[curr_direction]])
-                            heapq.heappush(q, bfs_map[x + dx][y + dy])
-                        # elif prev_cost == curr_cost:
-                        #     # print(f"(3) 기존 방향 {d}, 가려는 방향 {curr_direction} cost={curr_cost}")
-                        #     bfs_map[x + dx][y + dy][3].append(mapper[curr_direction])
-                        #     set(bfs_map[x + dx][y + dy][3])
-                        #     heapq.heappush(q, bfs_map[x + dx][y + dy])
-                            
-    return bfs_map[len(board)-1][len(board)-1][2] * 100
+    global cost
+    board[0][0]=1
+    direction = [(1,0),(-1,0),(0,1),(0,-1)]
+    length = len(board)
+    waycost = [[-1 for i in range(length)] for i in range(length)]
+    waycost[0][0] = 0
+    for d in direction :
+        newx = d[0]
+        newy = d[1]
+        if max(newx, newy) < length and min(newx, newy) >= 0 and board[newx][newy] == 0:
+            DFS(board, 100, d, newx, newy, length, waycost)
+            board[newx][newy] = 0
+    return cost
