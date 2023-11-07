88dcd228f1d7067d5f9fff9ffe223029585a753d
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-07 14:02:22 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -11,14 +11,14 @@ def DFS(x, y, direction, cost, board, cost_board):
     global answer
     
     board[x][y] = 1  # 방문
-    # cost_board[x][y] = cost
-    if cost_board[x][y] == -1 :
+    if cost_board[x][y] == -1 or cost_board[x][y] + 400 >= cost:
         cost_board[x][y] = cost
     else :
-        if cost_board[x][y] + 400 < cost:
-            return
-        else : 
-            cost_board[x][y] = cost
+        # 현재는 더 cost가 적더라도 최적의 해가 아닐 수 있음
+        # 예를 들어 앞서 다른 탐색에서 구한 cost1이 있고, 현재 새로 구한 cost2가 있다고 하자.
+        # 지금은 cost1 < cost2이지만, cost1 + 600 > cost2 + 100인 경우가 있을 수 있다.
+        # 따라서 두 식을 계산하면 cost1 < cost2 <= cost1 + 400인 cost2가 존재한다면 cost를 업데이트하고, 아니라면 얼리 리턴한다.
+        return
     
     # 도착
     if x == len(board) - 1 and y == len(board) - 1:
@@ -50,54 +50,13 @@ def solution(board):
     cost_board = [[-1 for _ in range(n)] for _ in range(n)]
     
     # 시작점 -> 오른쪽 진행 or 아래 진행
+    board[0][0] = 1
+    cost_board[0][0] = 0
     directions = [(0, 1), (1, 0)]
     for d in directions:
         nx, ny = d
         if board[nx][ny] == 0:
             DFS(nx, ny, d, COST_STRAIGHT, board, cost_board)
-            board[nx][ny] = 0
+            board[nx][ny] = 0  # 다른 탐색을 위해 rollback
     
     return answer
-
-
-
-
-# cost = 99999999999
-
-# def DFS(board, curmoney,direct,x,y, length, waycost) :
-#     global cost
-#     board[x][y] = 1
-#     if waycost[x][y] == -1 :
-#         waycost[x][y] = curmoney
-#     else :
-#         if waycost[x][y]+400 < curmoney :
-#             return
-#         else : 
-#             waycost[x][y] = curmoney
-#     if (x, y) == (length-1,length-1) :
-#         if cost>curmoney :
-#             cost = curmoney
-#         return
-#     direction = [(1,0),(0,1),(-1,0),(0,-1)]
-#     for d in direction :
-#         newx = x+d[0]
-#         newy = y +d[1]
-#         if max(newx,newy)<length and min(newx,newy) >=0 and board[newx][newy] == 0:
-#             newcurmoney = curmoney + 100 if direct == d else curmoney + 600
-#             DFS(board, newcurmoney, d, newx,newy, length, waycost)
-#             board[newx][newy] = 0
-
-# def solution(board):
-#     global cost
-#     board[0][0]=1
-#     direction = [(1,0),(-1,0),(0,1),(0,-1)]
-#     length = len(board)
-#     waycost = [[-1 for i in range(length)] for i in range(length)]
-#     waycost[0][0] = 0
-#     for d in direction :
-#         newx = d[0]
-#         newy = d[1]
-#         if max(newx, newy) < length and min(newx, newy) >= 0 and board[newx][newy] == 0:
-#             DFS(board, 100, d, newx, newy, length, waycost)
-#             board[newx][newy] = 0
-#     return cost
