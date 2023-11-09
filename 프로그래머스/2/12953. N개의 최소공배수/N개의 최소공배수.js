d0c3abb32cc3a35762637f6bc5184d41d726b67c
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-09 15:16:06 +0900

    [level 2] Title: N개의 최소공배수, Time: 87.75 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,3 +1,27 @@
+// function getLCM(a, b) {
+//     if (b % a === 0) {
+//         return b
+//     }
+    
+//     max = a * b
+//     for (let i = b; i <= max; i++) {
+//         if (i % a === 0 && i % b === 0) {
+//             return i
+//         }
+//     }
+// }
+
+// function solution(arr) {
+//     arr.sort((a, b) => a < b)
+    
+//     let currLCM = arr[0]
+//     for (let i = 0; i < arr.length - 1; i++) {
+//         currLCM = getLCM(currLCM, arr[i+1])
+//     }
+    
+//     return currLCM
+// }
+
 function getLCM(a, b) {
     if (b % a === 0) {
         return b
@@ -12,12 +36,15 @@ function getLCM(a, b) {
 }
 
 function solution(arr) {
-    arr.sort((a, b) => a < b)
-    
-    let currLCM = arr[0]
-    for (let i = 0; i < arr.length - 1; i++) {
-        currLCM = getLCM(currLCM, arr[i+1])
+    arr.sort((a, b) => a - b)
+
+    while (arr.length >= 2) {
+        let a = arr.pop();
+        let b = arr.pop();
+        
+        arr.push(getLCM(a, b))
+        console.log(arr)
     }
     
-    return currLCM
+   return arr[0]
 }
