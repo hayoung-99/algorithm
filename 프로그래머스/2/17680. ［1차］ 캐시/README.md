6fc0cf886e8f6dfbd33a4b4e8b788a79459a0f6e
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-11 10:47:00 +0900

    [level 2] Title: [1차] 캐시, Time: 0.50 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=javascript#) 
 
 ### 성능 요약
 
-메모리: 37.2 MB, 시간: 6.35 ms
+메모리: 33.4 MB, 시간: 0.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 10:29:2
+2023년 11월 6일 10:46:58
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,47 +1,27 @@
-const CACHE_HIT = 1
-const CACHE_MISS = 5
-
-function solution(cacheSize, cities) {    
+const MISS = 5, HIT = 1
+function solution(cacheSize, cities) {
     if (cacheSize === 0) {
-        return (cities.length * CACHE_MISS)
+        return (cities.length * MISS)
     }
     
     const cache = []
-    let total_time = 0
-    let used_time = 0
+    let answer = 0
     
-    for (const curr of cities) {
-        const city = curr.toUpperCase()
-        if (cache.length === 0) {
-            cache.push([city, used_time++])
-            total_time += CACHE_MISS
+    cities.forEach((item) => {
+        const city = item.toUpperCase()
+        const idx = cache.indexOf(city)
+        
+        if (idx === -1) {
+            if (cache.length === cacheSize) {
+                cache.shift()
+            }
+            answer += MISS
         } else {
-            let isInCache = false
-            for (let i = 0; i < cache.length; i++) {
-                if (cache[i][0] === city) {
-                    total_time += CACHE_HIT
-                    isInCache = true
-                    cache[i] = [city, used_time++]
-                }
-            }
-            if (!isInCache) {
-                if (cache.length < cacheSize) {
-                    cache.push([city, used_time++])
-                } else {
-                    // 가장 오랫동안 쓰지 않은 캐시 교체
-                    let most_stale_time = cities.length
-                    let most_stale_idx
-                    for (let i = 0; i < cache.length; i++) {
-                        if (most_stale_time > cache[i][1]) {
-                            most_stale_time = cache[i][1]
-                            most_stale_idx = i
-                        }
-                    }
-                    cache[most_stale_idx] = [city, used_time++]
-                }
-                total_time += CACHE_MISS
-            }
-        } 
-    }
-    return total_time
+            cache.splice(idx, 1)
+            answer += HIT
+        }
+        cache.push(city)
+    })
+    
+    return answer
 }
