a3f0bd4da5d677a6bb8bf6bc9c475aa37b09da7e
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-12 11:28:45 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 1558.41 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.js
@@ -0,0 +1,47 @@
+// 1. 가장 처음으로 오는 0을 1로 설정한다. 
+// 2. 그 뒤의 (뒤가 있다면) 1을 0으로 바꾼다.
+function getBit(n) {
+    let temp = ''
+    
+    while (n) {
+        temp = temp + (n % 2)
+        n = parseInt(n / 2)
+    }
+    
+    // reverse
+    return '0' + temp.split("").reverse().join("")
+}
+
+
+function getDecimal(bit) {
+    let answer = 0
+    let pow = 0
+    for (let i = bit.length - 1; i >= 0; i--) {
+        answer += Math.pow(2, pow) * bit[i]
+        pow += 1
+    }
+    
+    return answer
+}
+
+
+function solution(numbers) {
+    const answer = [];
+    
+    for (const num of numbers) {
+        const bit = getBit(num)
+        
+        // 1.
+        const zero_idx = bit.lastIndexOf('0')
+        let curr = bit.substring(0, zero_idx) + '1' + bit.substring(zero_idx + 1, bit.length)
+        
+        // 2.
+        if (zero_idx !== bit.length - 1) {
+            curr = curr.substring(0, zero_idx + 1) + '0' + curr.substring(zero_idx + 2, bit.length)
+        }
+        
+        answer.push(getDecimal(curr))
+    }
+    
+    return answer;
+}
