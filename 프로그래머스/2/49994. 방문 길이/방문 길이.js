7204b4146403bd039e6d62f61c481b8c84fdfdd1
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-12 12:07:13 +0900

    [level 2] Title: 방문 길이, Time: 0.50 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -2,6 +2,7 @@ function inRange(x, y) {
     return ((-5 <= x && x <= 5) && (-5 <= y && y <= 5))
 }
 
+
 function solution(dirs) {
     const visited = []
     for (let i = 0; i < 11; i++) {
@@ -28,7 +29,6 @@ function solution(dirs) {
     for (const dir of dirs) {
         const [dx, dy] = mapper[dir]
         const nx = curr_x + dx, ny = curr_y + dy
-        // console.log(`(${curr_x}, ${curr_y}) -> (${nx}, ${ny})`)
         
         // 더 이상 갈 수 없을 때
         if (!inRange(nx, ny)) {
@@ -37,8 +37,8 @@ function solution(dirs) {
         
         // 처음 방문할 때
         if (!visited[curr_x + 5][curr_y + 5][visited_mapper[dir]]) {
-            // console.log(`처음 가는 길: (${curr_x}, ${curr_y}) -> (${nx}, ${ny})`)
             answer += 1
+            
             // 정방향
             visited[curr_x + 5][curr_y + 5][visited_mapper[dir]] = 1
             // 역방향
