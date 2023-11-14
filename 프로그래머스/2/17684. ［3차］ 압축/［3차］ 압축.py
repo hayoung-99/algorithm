761357151e6cd5a65e8f78fab8a9dc2f3f05fd5e
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-15 03:41:50 +0900

    [level 2] Title: [3차] 압축, Time: 0.18 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -6,24 +6,23 @@ def solution(msg):
     for i in range(ord('A'), ord('Z') + 1):
         dt[chr(i)] = i - ord('A') + 1
         
-    curr = msg[0]
-    idx = 1
-    add_num = 27
-    while idx < len(msg):
-        if curr in dt:
-            curr += msg[idx]
-            idx += 1
+    idx_num = 27  # 색인 번호
+    for i in range(0, len(msg)):
+        if i == 0:
+            curr = msg[0]
+            continue
+            
+        next_msg = curr + msg[i]
+        if next_msg in dt:
+            curr = next_msg
             
         else:
-            answer.append(dt[curr[:-1]])
-            dt[curr] = add_num
-            add_num += 1
-            curr = curr[-1]
+            answer.append(dt[curr])
+            dt[next_msg] = idx_num
+            idx_num += 1
+            curr = msg[i]
 
     # 마지막으로 처리되지 않은 글자 처리
-    if curr not in dt:
-        answer.append(dt[curr[:-1]])
-        curr = curr[-1]
     answer.append(dt[curr])
 
     return answer
