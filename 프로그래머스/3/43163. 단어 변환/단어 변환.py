995729ad77e6dc44b038eff9970dc91868cce6cf
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-23 01:50:12 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,41 +1,29 @@
-def checkValid(word, target):
-    diff = 0
-    for i in range(len(word)):
-        if word[i] != target[i]:
-            diff += 1
-        if diff > 1:
-            return False
-    return True
-            
-
-def makeComb(target, comb, words):
-    global answer
-    
-    # 현재 word와 target 비교
-    # 현재 comb 다음에 target이 올 수 있다면 더 탐색하지 않아도 되므로 종료
-    last_word = comb[-1]
-    if checkValid(last_word, target):
-        print(comb)
-        answer = min(answer, len(comb))
-        return 
-    
+def getNextWord(curr, words):
     for word in words:
-        copy = list(words)
-        
-        if checkValid(last_word, word):
-            comb.append(word)
-            copy.remove(word)
-            makeComb(target, comb, copy)
-            comb.pop()
-    
+        cnt = 0
+        for c, w in zip(curr, word):
+            if c != w:
+                cnt += 1
+                
+        if cnt == 1:
+            yield word
 
+            
 def solution(begin, target, words):
-    global answer
-    answer = 50
-    
     if target not in words:
         return 0
     
-    makeComb(target, [begin], words)
+    levels = {begin: 0}
+    q = [begin]
     
-    return answer
+    while q:
+        curr = q.pop(0)
+        for nextWord in getNextWord(curr, words):
+            if nextWord not in levels:
+                levels[nextWord] = levels[curr] + 1
+                q.append(nextWord)
+                
+    if target not in levels:
+        return 0
+    return levels[target]
+    
