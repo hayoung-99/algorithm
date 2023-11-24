7f870ef246d96a8e3169b81f268962f4720cbe39
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-25 00:51:54 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 4.91 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,17 @@
+import re
+
+def solution(files):
+    head_and_number = []
+    for i, file in enumerate(files):
+        # head, number 추출
+        strings = re.split(r'[0-9]', file)
+        numbers = [num for num in re.split(r'[^0-9]', file) if num != '']
+        
+        head_and_number.append((strings[0], int(numbers[0]), i))
+
+    head_and_number.sort(key=lambda x: (x[0].upper(), x[1]))
+    answer = []
+    for han in head_and_number:
+        answer.append(files[han[2]])
+        
+    return answer
