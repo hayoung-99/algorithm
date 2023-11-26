5adb71a4ffcb16a8886edcfb5dd877327f1ede96
hayoung-99 <78773781+hayoung-99@users.noreply.github.com>
2023-11-27 03:13:44 +0900

    [level 2] Title: [3차] 방금그곡, Time: 15.92 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -0,0 +1,75 @@
+import re
+
+def calculatePlaytime(s, e):
+    s_hour, s_minute = map(int, s.split(':'))
+    e_hour, e_minute = map(int, e.split(':'))
+    
+    return (e_hour * 60 + e_minute) - (s_hour * 60 + s_minute)
+
+
+def notesToArray(notes):
+    arr = []
+    idx = 0
+    
+    while idx < len(notes):
+        if idx + 1 < len(notes) and notes[idx + 1] == '#':
+            arr.append(notes[idx:idx+2])
+            idx += 2
+        else:
+            arr.append(notes[idx])
+            idx += 1
+            
+    return arr
+        
+    
+def findall(target, notes):
+    answer = []
+    
+    idx = 0
+    while idx < len(notes):
+        if notes[idx: idx + len(target)] == target:
+            answer.append(idx)
+            idx += len(target)
+        else:
+            idx += 1
+            
+        if idx + len(target) > len(notes):
+            break
+            
+    return answer
+    
+
+def solution(m, musicinfos):
+    musics = []
+    for mi in musicinfos:
+        s, e, name, notes = mi.split(',')
+        
+        # 재생 시간만큼 notes 반복
+        playtime = calculatePlaytime(s, e)
+        notes = notesToArray(notes)
+        
+        if playtime < len(notes):
+            played_notes = ''.join(notes[:playtime])
+        else:
+            played_notes = list(notes)
+            while playtime > len(played_notes):
+                played_notes += notes
+            played_notes = ''.join(played_notes[:playtime])
+
+        offsets = findall(m, played_notes)
+        if not offsets:
+            continue
+            
+        # 뭔가 여기서 문제인듯..
+        for offset in offsets:
+            if offset + len(m) <= len(played_notes):
+                if offset + len(m) == len(played_notes) or played_notes[offset + len(m)] != '#':
+                    musics.append((playtime, name))
+        
+    musics.sort(key=lambda x: -x[0])
+    
+    if not musics:
+        return "(None)"
+    return musics[0][1]
+        
+        
